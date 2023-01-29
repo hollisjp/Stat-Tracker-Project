e15849fdd7d2fb0207228caf1b82e0da4cea5994
hollisjp <119460688+hollisjp@users.noreply.github.com>
2023-01-28 21:03:32 -0600

    Add files via upload

---
+++ CodStatTracker.py
@@ -0,0 +1,73 @@
+import PIL
+from PIL import Image, ImageOps, ImageEnhance, ImageDraw
+import pyautogui as pg
+import pandas as pd
+import numpy as np
+import easyocr
+
+def imagePrep(path):
+    
+    #Take image and crop into a vertical row of pixels on the left of the scoreboard
+    imgOriginal = Image.open(path)
+    # imgOriginal.show()
+
+    enhancer = ImageEnhance.Brightness(imgOriginal)
+    enhancer2= ImageEnhance.Contrast(imgOriginal)
+    imgOriginal = enhancer.enhance(1.1)
+    imgOriginal = enhancer2.enhance(1.5)
+
+    imgFindPlayer = imgOriginal.crop((330,330,331,800))
+
+    #Convert string of pixels into an array and find the top and bottom white pixels
+    na = np.array(imgFindPlayer)
+    Y,X= np.where(np.all(na==[255,255,255],axis=2))
+    top, bottom = Y[0], Y[-1]
+
+    #Crop original image to just the player's row
+    imgNums = imgOriginal.crop((600,330+top,1450,top+365))
+    imgNums.save(r'C:\Users\Hollis\Desktop\scoreboardcropped.png')
+    # imgNums.show()
+
+    #Crop image for column names
+    imgCols = imgOriginal.crop((760,330,1450,380))
+    imgCols.save(r'C:\Users\Hollis\Desktop\scoreboardcroppedcolumns.png')
+    # imgCols.show()
+
+    reader = easyocr.Reader(['en'])
+    playerResult = reader.readtext(r'C:\Users\Hollis\Desktop\scoreboardcropped.png', detail = 0, text_threshold= .5, low_text=.001, contrast_ths=1)
+    playerResult = playerResult[0].split()
+    playerResult = [float(i) for i in playerResult]
+    print(playerResult)
+    columnsResult = reader.readtext(r'C:\Users\Hollis\Desktop\scoreboardcroppedcolumns.png', detail = 0, text_threshold= .9, low_text=.3)
+    columnsResult = [i.split() for i in columnsResult]
+    columnsResult = [i for j in columnsResult for i in j if i.strip().isalpha()]
+    print(columnsResult)
+
+    statColumns = ['DATE', 'SCORE', 'KILLS', 'DEATHS', 'CONFIRMS', 'DENIES', 'TIME', 'ASSISTS', 'LATENCY', 'PLANTS', 'DEFUSES']
+ 
+    masterData = pd.read_csv(r'C:\Users\Hollis\Documents\CodStats\codStats.csv')
+
+    statsData = pd.DataFrame(columns=columnsResult)
+    statsData.loc[len(statsData)+1] = playerResult
+    # statsData.head()
+    # masterData = pd.DataFrame()
+    masterData = pd.concat([statsData, masterData], axis=0, ignore_index=True)
+
+    k = int(masterData['KILLS'].sum())
+    d = int(masterData['DEATHS'].sum())
+    kd = k/d
+    print(f'KD: {kd}')
+    avgScore = masterData['SCORE'].mean()
+    print(f'Average Score: {avgScore}')
+
+    masterData.to_csv(r'C:\Users\Hollis\Documents\CodStats\codStats.csv', index=False)
+
+    print(masterData)
+    
+if __name__ == '__main__':
+    grab = pg.screenshot()
+    grab.save(r'C:\Users\Hollis\Desktop\tmp.png')
+    imagePrep(r'C:\Users\Hollis\Desktop\tmp.png')
+
+
+    print(sum(i for i in range(3,int(input())*3,3)if iin(i*5,i*7)))
